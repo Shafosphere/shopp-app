aeafd2cb235eb07c5576e9e5ea24ab058bbebeb4
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-04-08 21:53:38 +0200

    Version 0.4 - Added IMG Slider

---
+++ src/components/image-carousel/carousel.jsx
@@ -0,0 +1,27 @@
+import "./style-carousel.css"
+export default function ImageCarousel() {
+    return (
+        <div className="slider">
+            <div className="slide-track">
+                <div className="slide">
+                    <img alt="phone" src="https://cdn.dummyjson.com/product-images/4/1.jpg" />
+                </div>
+                <div className="slide">
+                    <img alt="phone" src="https://cdn.dummyjson.com/product-images/4/1.jpg" />
+                </div>
+                <div className="slide">
+                    <img alt="phone" src="https://cdn.dummyjson.com/product-images/4/1.jpg" />
+                </div>
+                <div className="slide">
+                    <img alt="phone" src="https://cdn.dummyjson.com/product-images/4/1.jpg" />
+                </div>
+                <div className="slide">
+                    <img alt="phone" src="https://cdn.dummyjson.com/product-images/4/1.jpg" />
+                </div>
+                <div className="slide">
+                    <img alt="phone" src="https://cdn.dummyjson.com/product-images/4/1.jpg" />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,12 +1,16 @@
 import "./styles.css"
 import Navbar from "../navbar/navbar"
 import Banner from "../banner/banner"
+import ImageCarousel from "../image-carousel/carousel"
 export default function Main(){
     return (
         <div className="container kantumruy">
             <div className="main-window">
                 <Navbar></Navbar>
                 <Banner></Banner>
+                <ImageCarousel></ImageCarousel>
+                <Banner></Banner>
+                <ImageCarousel></ImageCarousel>
             </div>
         </div>        
     )

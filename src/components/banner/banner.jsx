ef72399fd8765ea5c7a6163b36d1ed627a05ac6e
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-04-08 20:02:13 +0200

    Version 0.3 - Added Banner

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,8 @@
+import "./style-banner.css"
+export default function Banner(){
+    return(
+        <div className="container-banner">
+            <span>check it out!</span>
+        </div>
+    )
+}
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,10 +1,12 @@
 import "./styles.css"
 import Navbar from "../navbar/navbar"
+import Banner from "../banner/banner"
 export default function Main(){
     return (
         <div className="container kantumruy">
             <div className="main-window">
                 <Navbar></Navbar>
+                <Banner></Banner>
             </div>
         </div>        
     )

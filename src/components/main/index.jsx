6c13cc9c36f22fe33df0c2c33d8925cd902ee3c7
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2024-04-08 19:49:20 +0200

    Version 0.2 - Added Navbar

---
+++ src/components/main/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+import Navbar from "../navbar/navbar"
+export default function Main(){
+    return (
+        <div className="container kantumruy">
+            <div className="main-window">
+                <Navbar></Navbar>
+            </div>
+        </div>        
+    )
+}
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,81 @@
+import "./styles-navbar.css"
+import { FiShoppingCart } from "react-icons/fi";
+import { IoTriangle } from "react-icons/io5";
+
+import { PL } from 'country-flag-icons/react/3x2'
+import { useState } from "react";
+
+export default function Navbar() {
+    return (
+        <div className="container-navbar">
+            <div className="top-navbar">
+
+                <div className="title-input-container">
+                    {/* title */}
+                    <div className="title-navbar">SHOPP APP</div>
+                    {/* input */}
+                    <div className="input-navbar">
+                        <input className="input" type="text" placeholder="I want..." />
+                    </div>
+                </div>
+
+                <div className="money-navbar">
+                    {/* currency */}
+                        <CurrencyNavbar />
+                    {/* cart */}
+                    <div className="cart-navbar">
+                        <FiShoppingCart />
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="bot-navbar">
+                
+            <div className="categories">
+                CATEGORIES
+            </div>
+            <div className="categories-list">
+                <div className="">smartphones </div>
+                <div className="">laptops</div>
+                <div className="">SKINCARE</div>
+                <div className="">furniture</div>
+            </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+
+function CurrencyNavbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    function handleMenuClick() {
+        setIsMenuOpen(true);
+    }
+    function handleMouseOut() {
+        setIsMenuOpen(false);
+    }
+    return (
+        <div className="currency-navbar">
+            <div className="currency-top">
+                <PL className="currency-flag" />
+                <span className="currency-text">PLN / PL</span>
+            </div>
+            <div className="currency-bot" onMouseOver={handleMenuClick} onMouseOut={handleMouseOut} >
+                <IoTriangle className="currency-icon" />
+            </div>
+            {isMenuOpen && (
+                <div className="currency-menu">
+                    <ul>
+                        <li>one</li>
+                        <li>two</li>
+                        
+                    </ul>
+                </div>
+            )}
+        </div>
+    )
+}
